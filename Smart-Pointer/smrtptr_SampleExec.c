af06f8aaf95b4092b612e0bfe24e4d24e8d0e4a9
Jordan Randleman <jrandleman@scu.edu>
2019-08-21 15:58:49 -0700

    demos "smrtrealloc()"

--- Smart-Pointer/smrtptr_SampleExec.c
+++ Smart-Pointer/smrtptr_SampleExec.c
@@ -1,4 +1,4 @@
-// AUTHOR: JORDAN RANDLEMAN -- smrtptr_SampleExec.c -- smrtptr.h library sample execution
+// AUTHOR: JORDAN RANDLEMAN -- smrtptr_SampleExec.c -- smrtptr.h Library Sample Execution
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,22 +6,35 @@
 #include "smrtptr.h"
 
 /*
- * "smrtptr.h" provides 4 functions to handle pointers:
- * (1) smrtptr()    -- pass an allocated pointer argument    -- freed atexit
- * (2) smrtmalloc() -- treat like malloc (passing size)      -- freed atexit
- * (3) smrtcalloc() -- treat like calloc (passing num, size) -- freed atexit
- * (4) smrtfree()   -- treat like free -- frees ptr immediately & not atexit
+ * "smrtptr.h" provides 5 functions to handle pointers:
+ * (1) smrtptr()     -- pass an allocated pointer argument     -- freed atexit
+ * (2) smrtmalloc()  -- treat like malloc (passing size)       -- freed atexit
+ * (3) smrtcalloc()  -- treat like calloc (passing num, size)  -- freed atexit
+ * (4) smrtrealloc() -- treat like realloc (passing ptr, size) -- freed atexit
+ *                      compatible w/ both "smart" & "dumb" pointers!
+ * (5) smrtfree()    -- treat like free -- frees ptr immediately, & not atexit
  */
 
 int main() {
-  char *str = malloc(sizeof(char) * 5); // convert an allocated pointer to a smart pointer
+  // convert an allocated "dumb" pointer to a "smart" pointer
+  char *str = malloc(sizeof(char) * 5);
   smrtptr(str); 
 
-  int *nums = smrtmalloc(sizeof(int) * 3); // malloc a smart pointer
+  // malloc a smart pointer
+  int *nums = smrtmalloc(sizeof(int) * 3); 
 
-  long *bigNums = smrtcalloc(8, sizeof(long)); // calloc a smart pointer
+  // calloc a smart pointer
+  long *bigNums = smrtcalloc(8, sizeof(long)); 
 
-  smrtfree(nums); // free a smart pointer prior to atexit
+  // free a smart pointer immediately, prior to atexit
+  smrtfree(nums); 
+
+  // realloc a smart pointer
+  str = smrtrealloc(str, sizeof(char) * 20);
+
+  // malloc a dumb pointer, then realloc & convert it to a smart pointer
+  char *temp = malloc(sizeof(char) * 8);
+  temp = smrtrealloc(temp, sizeof(char) * 20);
 
   return 0;
 }
