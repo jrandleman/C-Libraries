2a6bcd49f7210a6afcb3c4393f14f4206ed91d34
Jordan Randleman <jrandleman@scu.edu>
2019-08-27 14:59:35 -0700

    added smrtassert

--- Smart-Pointer/smrtptr_SampleExec.c
+++ Smart-Pointer/smrtptr_SampleExec.c
@@ -2,17 +2,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+// #define NOISYSMRTPTR // tell smrtptr.h to printf how many smrtptrs freed atexit
+// #define NDEBUG       // tells smrtptr.h to disable all "smrtassert()" expressions
 #include "smrtptr.h"
 
-/*
- * "smrtptr.h" provides 5 functions to handle pointers:
+/**
+ * "smrtptr.h" provides 5 functions & a macro to handle pointers:
+ *
  * (1) smrtptr()     -- pass an allocated pointer argument     -- freed atexit
+ *
  * (2) smrtmalloc()  -- treat like malloc (passing size)       -- freed atexit
+ *
  * (3) smrtcalloc()  -- treat like calloc (passing num, size)  -- freed atexit
+ *
  * (4) smrtrealloc() -- treat like realloc (passing ptr, size) -- freed atexit
- *                      compatible w/ both "smart" & "dumb" pointers!
+ *                      => compatible w/ both "smart" & "dumb" pointers!
+ *
  * (5) smrtfree()    -- treat like free -- frees ptr immediately, & not atexit
+ *                      => put "#define NOISYSMRTPTR" B4 "#includ smrtptr.h" 
+ *                         to show how many smart pointers were freed
+ *
+ * (6) smrtassert()  -- performs like assert.h, but frees smrtptrs prior exit
+ *                   -- also like assert.h, put "#define NDEBUG" above 
+ *                      "#include smrtptr.h" to deactivate all "smrtassert"'s
  */
 
 int main() {
@@ -35,6 +48,10 @@ int main() {
   // malloc a dumb pointer, then realloc & convert it to a smart pointer
   char *temp = malloc(sizeof(char) * 8);
   temp = smrtrealloc(temp, sizeof(char) * 20);
+
+  // assert that temp != NULL, freeing all smart pointers & exiting if not
+  // in this case smrtrealloc actually checks this for us, but for demo's sake
+  smrtassert(temp != NULL);
 
   return 0;
 }
