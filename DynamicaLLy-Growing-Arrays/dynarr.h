8a78bccb93de92738ec42d9b4cb8be14a31bbabc
Jordan Randleman <jrandleman@scu.edu>
2019-09-11 18:27:44 -0700

    Updated Function Invocation Names & Documentation

--- DynamicaLLy-Growing-Arrays/dynarr.h
+++ DynamicaLLy-Growing-Arrays/dynarr.h
@@ -1,4 +1,37 @@
 /* AUTHOR: JORDAN RANDLEMAN -:- USE DOUBLY LL TO SIMULATE DYNAMIC ARRAY CREATION */
+/***
+ * RESERVED INTEGERS:
+ *   => 32202 ("ERROR")
+ *   => 8080808 (EMPTY INITIAL VALUE)
+ * RESERVED STRINGS:
+ *   => "32202" ("ERROR")
+ *   => "8080808" (EMPTY INITIAL VALUE)
+ *
+ * CLIENT-SIDE DYNAMIC ARRAY INTERFACE (fcn names indicate args as well as actions):
+ *   => NOTE: da = dynamic arr, dma = dynamic multi-dim array, idx = index, elt = element
+ *   => DYNAMIC ARRAY
+ *      - DA_INIT        // given a DYN_AR pointer & either 'i' or 'c', initializes dynamic array
+ *      - DA_DNIT        // given a DYN_AR pointer, frees all alocated memory (destroying dynamic array)
+ *      - da_length      // given da: returns current number of cells
+ *      - da_idx_of      // given da, idx, elt: returns "idx" of first "elt" instance in "da"
+ *      - da_idx_insert  // given da, idx, elt: allocs & inserts a new "da" cell at "idx" w/ "elt" value
+ *      - da_idx_assign  // given da, idx, elt: changes "da"'s current "idx" value to "elt"
+ *      - da_idx_delete  // given da & idx: returns 1 success or 0 fail for deleting "da" cell at "idx"
+ *      - da_idx_swap    // given da, idx1, & idx2: returns 1 success or 0 fail for swapping elts at "idx1" & "idx2"
+ *      - da_idx_ival    // given da & idx, returns int "elt" at "da"'s "idx"
+ *      - da_idx_cval    // given da & idx, returns char* "elt" at "da"'s "idx"
+ *   => DYNAMIC MULTI-DIMENSIONAL ARRAY
+ *      - DMA_INIT       // given a DYN_MAR pointer, number of dynamic arrays, & either 'i' or 'c', initializes dynamic array
+ *      - DMA_DNIT       // given a DYN_MAR pointer & number of dynamic arrays, frees all alocated memory (destroying dynamic array)
+ *      - dma_length     // see da_length, but w/ "dma" instead of "da"
+ *      - dma_idx_of     // see da_idx_of, but w/ "subscripted dma" instead of "da"
+ *      - dma_idx_insert // see da_idx_insert, but w/ "subscripted dma" instead of "da"
+ *      - dma_idx_assign // see da_idx_assign, but w/ "subscripted dma" instead of "da"
+ *      - dma_idx_delete // see da_idx_delete, but w/ "subscripted dma" instead of "da"
+ *      - dma_idx_swap   // see da_idx_swap, but w/ "subscripted dma" instead of "da"
+ *      - dma_idx_ival   // ee da_idx_ival, but w/ "subscripted dma" instead of "da"
+ *      - dma_idx_cval   // ee da_idx_cval, but w/ "subscripted dma" instead of "da"
+ */
 
 #ifndef DYNARR_H_
 #define DYNARR_H_
@@ -9,12 +42,15 @@
 /* n0() => RVAL NUMBER ADDRESS -:- v4() => RVAL VAR ADDRESS [n0() W/ STR'S] */
 #define n0(MAC_NUM_VAL) ({int MAC_NUM_ADDRESS = MAC_NUM_VAL;&MAC_NUM_ADDRESS;})
 #define v4(v4_ELEM,v4_TYPE) ({int v44MP = ((v4_TYPE == 1)*(int)v4_ELEM);(void*)&v44MP;})
-#define _idx(i_DYNA,i_ELIM) ((i_DYNA->da_type == 0)?(_insert_da(i_DYNA,(void*)i_ELIM)):(_insert_da(i_DYNA,v4(i_ELIM,i_DYNA->da_type))))
-#define _put(p_DYNA,p_IDX,p_ELIM) ((p_DYNA->da_type == 0)?(_put_da(p_DYNA,p_IDX,(void*)p_ELIM)):(_put_da(p_DYNA,p_IDX,v4(p_ELIM,p_DYNA->da_type))))
-#define _add(a_DYNA,a_IDX,a_ELIM) ((a_DYNA->da_type == 0)?(_add_da(a_DYNA,a_IDX,(void*)a_ELIM)):(_add_da(a_DYNA,a_IDX,v4(a_ELIM,a_DYNA->da_type))))
-#define _idxm(i_DYMA,im_ELIM) (_idx(i_DYMA.d4,im_ELIM))
-#define _putm(p_DYMA,pm_IDX,pm_ELIM) (_put(p_DYMA.d4,pm_IDX,pm_ELIM))
-#define _addm(a_DYMA,am_IDX,am_ELIM) (_add(a_DYMA.d4,am_IDX,am_ELIM))
+#define da_idx_of(i_DYNA,i_ELIM)\
+	((i_DYNA->da_type == 0)?(dynamicarr_index_of(i_DYNA,(void*)i_ELIM)):(dynamicarr_index_of(i_DYNA,v4(i_ELIM,i_DYNA->da_type))))
+#define da_idx_assign(p_DYNA,p_IDX,p_ELIM)\
+	((p_DYNA->da_type == 0)?(dynamicarr_index_assign(p_DYNA,p_IDX,(void*)p_ELIM)):(dynamicarr_index_assign(p_DYNA,p_IDX,v4(p_ELIM,p_DYNA->da_type))))
+#define da_idx_insert(a_DYNA,a_IDX,a_ELIM)\
+	((a_DYNA->da_type == 0)?(dynamicarr_index_push(a_DYNA,a_IDX,(void*)a_ELIM)):(dynamicarr_index_push(a_DYNA,a_IDX,v4(a_ELIM,a_DYNA->da_type))))
+#define dma_idx_of(i_DYMA,im_ELIM) (da_idx_of(i_DYMA.d4,im_ELIM))
+#define dma_idx_assign(p_DYMA,pm_IDX,pm_ELIM) (da_idx_assign(p_DYMA.d4,pm_IDX,pm_ELIM))
+#define dma_idx_insert(a_DYMA,am_IDX,am_ELIM) (da_idx_insert(a_DYMA.d4,am_IDX,am_ELIM))
 /******************************************************************************
 * DYNA LL NODE 'CELL' STRUCTURE W/ UNION FOR CORRESPONDING INT/CHAR DATA TYPE
 ******************************************************************************/
@@ -44,7 +80,7 @@ typedef struct dyn_mar { /* declare: DYN_MAR userDynMultiArrName[USER_SIZE]; */
 ******************************************************************************/
 void DA_INIT(DYN_AR *u_da, char u_da_type) { /* init: DA_INIT(userDynArrName, 'i'/'c') */
 	if(u_da_type != 'i' && u_da_type != 'I' && u_da_type != 'c' && u_da_type != 'C') {
-		printf("\n\nINCOMPATIBLE DYNAMIC ARRAY TYPE\n=> ENTER 'i' ");
+		printf("\n\n\033[1m\033[31mERROR\033[0m INCOMPATIBLE DYNAMIC ARRAY TYPE\n=> ENTER 'i' ");
 		printf("FOR INT ARRAY\n=> ENTER 'c' FOR CHAR/STR ARRAY\n\n");
 		exit(0);
 	}
@@ -79,7 +115,7 @@ void DMA_DNIT(DYN_MAR *u_da) { for(int i = 0; i < (u_da -> dyn_mar_size); i++) D
 /******************************************************************************
 * 'L'ENGTH ==> return length of current LL
 ******************************************************************************/
-int _len(DYN_AR *u_da) {
+int da_length(DYN_AR *u_da) {
 	int count = 0;
 	DYNA *p = u_da -> da_head;
 	if(p != NULL) {
@@ -93,7 +129,7 @@ int _len(DYN_AR *u_da) {
 /******************************************************************************
 * 'I'NDEX ==> return: first index with element true, -1 DNE
 ******************************************************************************/
-int _insert_da(DYN_AR *u_da, void *value) {
+int dynamicarr_index_of(DYN_AR *u_da, void *value) {
 	int count = 0;
 	DYNA *p = u_da -> da_head;
 	if(p != NULL) {
@@ -112,7 +148,7 @@ int _insert_da(DYN_AR *u_da, void *value) {
 /******************************************************************************
 * 'E'LEM ==> return: index's elem true, 32202/"32202" DNE ==> -index backwards
 ******************************************************************************/
-void* _ed(DYN_AR *u_da, int index, int elim_val_type) { 
+static void* da_return_index_value(DYN_AR *u_da, int index, int elim_val_type) { 
 	int count = 0, i, err_num = 32202;
 	void *err_val = &err_num, *gud_val;
 	DYNA *p;
@@ -128,7 +164,7 @@ void* _ed(DYN_AR *u_da, int index, int elim_val_type) {
 				return (void*)"32202";
 			}
 		if(p == NULL || p == u_da -> da_tail) {
-			if(elim_val_type == 1 && p -> elim.ida_val == 80808) {
+			if(elim_val_type == 1 && p -> elim.ida_val == 8080808) {
 				return err_val;
 			} else if(elim_val_type == 0 && p -> elim.cda_val == '\0') {
 				return (void*)"32202";
@@ -144,7 +180,7 @@ void* _ed(DYN_AR *u_da, int index, int elim_val_type) {
 				return (void*)"32202";
 			}
 		if(p == NULL) {
-			if(elim_val_type == 1 && p -> elim.ida_val == 80808) {
+			if(elim_val_type == 1 && p -> elim.ida_val == 8080808) {
 				return err_val;
 			} else if(elim_val_type == 0 && p -> elim.cda_val == '\0') {
 				return (void*)"32202";
@@ -159,12 +195,12 @@ void* _ed(DYN_AR *u_da, int index, int elim_val_type) {
 		return (void*)(u_da -> da_head -> elim.cda_val);
 	}
 }
-int _elti(DYN_AR *u_da, int index) { return *(int*)_ed(u_da, index, u_da -> da_type); }
-char* _eltc(DYN_AR *u_da, int index) { return _ed(u_da, index, u_da -> da_type); }
+int da_idx_ival(DYN_AR *u_da, int index) { return *(int*)da_return_index_value(u_da, index, u_da -> da_type); }
+char* da_idx_cval(DYN_AR *u_da, int index) { return da_return_index_value(u_da, index, u_da -> da_type); }
 /******************************************************************************
 * 'P'UT ==> put new value as index's element, return: 1 true, 0 DNE
 ******************************************************************************/
-int _put_da(DYN_AR *u_da, int index, void *value) {
+int dynamicarr_index_assign(DYN_AR *u_da, int index, void *value) {
 	int count = 0, i;
 	DYNA *p = u_da -> da_head;
 	for(i = 0; i < index; i++, p = p -> nixt)
@@ -180,33 +216,33 @@ int _put_da(DYN_AR *u_da, int index, void *value) {
 /******************************************************************************
 * 'S'WAP ==> swap idx1 & idx2 elements, return: 1 true, 0 DNE
 ******************************************************************************/
-int _swap(DYN_AR *u_da, int idx1, int idx2) {
+int da_idx_swap(DYN_AR *u_da, int idx1, int idx2) {
 	int value1, value2;
 	char *val1, *val2;
 	if (u_da -> da_type == 1) {
-		value1 = _elti(u_da, idx1);
+		value1 = da_idx_ival(u_da, idx1);
 		if(value1 == 32202) return 0;
-		value2 = _elti(u_da, idx2);
+		value2 = da_idx_ival(u_da, idx2);
 		if(value2 == 32202) return 0;
-		if(_put_da(u_da, idx1, &value2) == 0 || _put_da(u_da, idx2, &value1) == 0) return 0;
+		if(dynamicarr_index_assign(u_da, idx1, &value2) == 0 || dynamicarr_index_assign(u_da, idx2, &value1) == 0) return 0;
 	} else {
-		val1 = _eltc(u_da, idx1);
+		val1 = da_idx_cval(u_da, idx1);
 		if(strcmp(val1, "32202") == 0) return 0;
-		val2 = _eltc(u_da, idx2);
+		val2 = da_idx_cval(u_da, idx2);
 		if(strcmp(val2, "32202") == 0) return 0;
-		if(_put_da(u_da, idx1, val2) == 0 || _put_da(u_da, idx2, val1) == 0) return 0;
+		if(dynamicarr_index_assign(u_da, idx1, val2) == 0 || dynamicarr_index_assign(u_da, idx2, val1) == 0) return 0;
 	}
 	return 1;
 }
 /******************************************************************************
 * TRIM EMPTY CELLS AT THE END OF ARRAYS POST DELETION, ONLY KEEPING LAST BUFFER
 ******************************************************************************/
-void da_trim_cells_(DYN_AR *u_da) {
+static void dynamic_arr_trim_cells(DYN_AR *u_da) {
 	while(u_da -> da_tail -> priv != NULL) {
 		if(u_da -> da_type == 1) {
-			if(u_da -> da_tail -> priv -> elim.ida_val != 80808) return;
+			if(u_da -> da_tail -> priv -> elim.ida_val != 8080808) return;
 		} else {
-			if(strcmp(u_da -> da_tail -> priv -> elim.cda_val, "XQXQX") != 0) return;
+			if(strcmp(u_da -> da_tail -> priv -> elim.cda_val, "8080808") != 0) return;
 		}
 		DYNA *q = u_da -> da_tail;
 		u_da -> da_tail = u_da -> da_tail -> priv;
@@ -219,7 +255,7 @@ void da_trim_cells_(DYN_AR *u_da) {
 /******************************************************************************
 * 'D'ELETE ==> return: 1 true, 0 DNE ==> -index backwards
 ******************************************************************************/
-int _del(DYN_AR *u_da, int index) {
+int da_idx_delete(DYN_AR *u_da, int index) {
 	int count = 0, i;
 	DYNA *p;
 	if(u_da -> da_head == NULL) return 0;
@@ -248,16 +284,16 @@ int _del(DYN_AR *u_da, int index) {
 		u_da -> da_head = u_da -> da_head -> nixt;
 	}
 	free(p);
-	da_trim_cells_(u_da);
+	dynamic_arr_trim_cells(u_da);
 	return 1;
 }
 /******************************************************************************
 * 'A'DD ==> add value at index
 ******************************************************************************/
-void _add_da(DYN_AR *u_da, int index, void *value) {
+void dynamicarr_index_push(DYN_AR *u_da, int index, void *value) {
 	/* EMPTY CELL VALUES */
-	DYNA emptyi = { .nixt = NULL, .priv = NULL, .elim.ida_val = 80808 };
-	DYNA emptyc = { .nixt = NULL, .priv = NULL, .elim.cda_val = "XQXQX" };
+	DYNA emptyi = { .nixt = NULL, .priv = NULL, .elim.ida_val = 8080808 };
+	DYNA emptyc = { .nixt = NULL, .priv = NULL, .elim.cda_val = "8080808" };
 	DYNA *p = (DYNA *)malloc(sizeof(DYNA));
 	if(u_da -> da_type == 1) {
 		p -> elim.ida_val = *(int*)value;
@@ -324,10 +360,10 @@ void _add_da(DYN_AR *u_da, int index, void *value) {
 /******************************************************************************
 * MULTI ARRAY OF DYNAMIC ARRAYS FUNCTION COUNTERPARTS ==> function_name+'m'
 ******************************************************************************/
-int _lenm(DYN_MAR u_da) { return _len(u_da.d4); }
-int _eltim(DYN_MAR u_da, int index) { return *(int*)_ed(u_da.d4, index, u_da.d4 -> da_type); }
-char* _eltcm(DYN_MAR u_da, int index) { return _ed(u_da.d4, index, u_da.d4 -> da_type); }
-int _swapm(DYN_MAR u_da, int idx1, int idx2) { return _swap(u_da.d4, idx1, idx2); }
-int _delm(DYN_MAR u_da, int index) { return _del(u_da.d4, index); }
+int dma_length(DYN_MAR u_da) { return da_length(u_da.d4); }
+int dma_idx_ival(DYN_MAR u_da, int index) { return *(int*)da_return_index_value(u_da.d4, index, u_da.d4 -> da_type); }
+char* dma_idx_cval(DYN_MAR u_da, int index) { return da_return_index_value(u_da.d4, index, u_da.d4 -> da_type); }
+int dma_idx_swap(DYN_MAR u_da, int idx1, int idx2) { return da_idx_swap(u_da.d4, idx1, idx2); }
+int dma_idx_delete(DYN_MAR u_da, int index) { return da_idx_delete(u_da.d4, index); }
 
 #endif
